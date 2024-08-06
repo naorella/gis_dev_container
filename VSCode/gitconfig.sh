0463d340b31cedb2d476419b020c6be6a19ebf13
Nelson <131645782+naorella@users.noreply.github.com>
2024-08-06 12:04:21 -0400

    Update gitconfig.sh
    Update line for default code editor.
    --wait flag allows for VSCode to be aware of changes to files

--- VSCode/gitconfig.sh
+++ VSCode/gitconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git config --global user.name "user name"
 git config --global user.email username@email.com
-git config --global core.editor code
+git config --global core.editor "code --wait"
 git config --global init.defaultBranch main
